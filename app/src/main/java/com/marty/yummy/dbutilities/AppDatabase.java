908444f66220679644b27cff3af45625c7d917e0
Romix VR <111367434+romixvr@users.noreply.github.com>
2024-08-06 20:58:33 -0600

    Update AppDatabase.java
    Mejorando, aplicando sugerencia de SonarClouds

--- app/src/main/java/com/marty/yummy/dbutilities/AppDatabase.java
+++ app/src/main/java/com/marty/yummy/dbutilities/AppDatabase.java
@@ -1,22 +1,20 @@
 package com.marty.yummy.dbutilities;
 
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import com.marty.yummy.model.CartItem;
 import com.marty.yummy.model.FoodDetails;
 
-
-
 /**
  * Created by Marty on 12/29/2017.
  */
-@Database(entities = {FoodDetails.class, CartItem.class}, version = 2,exportSchema = false)
+@Database(entities = {FoodDetails.class, CartItem.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-    private static AppDatabase INSTANCE;
+    private static volatile AppDatabase INSTANCE;
 
     public abstract FoodDetailsDao foodDetailsDao();
     public abstract CartItemDao cartItemDao();
@@ -25,7 +23,11 @@ public abstract class AppDatabase extends RoomDatabase {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class,"yummy").fallbackToDestructiveMigration().allowMainThreadQueries().build();
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "yummy")
+                            // Implement appropriate migrations instead of destructive migration
+                            //.fallbackToDestructiveMigration()
+                            .build();
                 }
             }
         }
