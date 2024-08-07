7cb3f561117461c6ff38af21272dc8fd6efa455d
Romix VR <111367434+romixvr@users.noreply.github.com>
2024-08-06 21:22:52 -0600

    Update SaveFoodMenu.java
    Corrigiendo codigo 1

--- app/src/main/java/com/marty/yummy/worker/SaveFoodMenu.java
+++ app/src/main/java/com/marty/yummy/worker/SaveFoodMenu.java
@@ -1,36 +1,40 @@
 package com.marty.yummy.worker;
 
-import android.os.AsyncTask;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.marty.yummy.dbutilities.AppDatabase;
 import com.marty.yummy.model.FoodDetails;
 
-import java.util.ArrayList;
-import java.util.List;
+public class SaveFoodMenu {
 
-public class SaveFoodMenu extends AsyncTask<Void,Void,Void> {
-    private AppDatabase db;
-    private List<FoodDetails> foodDetails;
-    public SaveFoodMenu(AppDatabase db,List<FoodDetails> foodDetails) {
+    private final AppDatabase db;
+    private final List<FoodDetails> foodDetails;
+
+    public SaveFoodMenu(AppDatabase db, List<FoodDetails> foodDetails) {
         this.db = db;
         this.foodDetails = foodDetails;
     }
 
-    @Override
-    protected Void doInBackground(Void... voids) {
-        if(db!=null){
-            if(foodDetails!=null && foodDetails.size()>0) {
-                List<String> nameList = new ArrayList<>();
-                for (SomeType value : list) {
-                    nameList.add(foodDetails.get(i).getName());
-                    foodDetails.get(i).setQuantity(db.cartItemDao().getCartCount(foodDetails.get(i).getName()));
+    public void execute() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            if (db != null) {
+                if (foodDetails != null && !foodDetails.isEmpty()) {
+                    List<String> nameList = new ArrayList<>();
+                    for (FoodDetails foodDetail : foodDetails) {
+                        nameList.add(foodDetail.getName());
+                        foodDetail.setQuantity(db.cartItemDao().getCartCount(foodDetail.getName()));
+                    }
+                    db.foodDetailsDao().save(foodDetails);
+                    db.foodDetailsDao().deleteOtherFoods(nameList);
+                } else {
+                    db.foodDetailsDao().deleteAll();
                 }
-                db.foodDetailsDao().save(foodDetails);
-                db.foodDetailsDao().deleteOtherFoods(nameList);
-            }else{
-                db.foodDetailsDao().deleteAll();
             }
-        }
-        return null;
+        });
+        executor.shutdown();
     }
 }
