8d45de5426f993c48724f5bf73ed343f530d4285
Romix VR <111367434+romixvr@users.noreply.github.com>
2024-08-06 21:03:40 -0600

    Update FoodRepository.java
    Corrigiendo el codigo

--- app/src/main/java/com/marty/yummy/services/repository/FoodRepository.java
+++ app/src/main/java/com/marty/yummy/services/repository/FoodRepository.java
@@ -1,8 +1,9 @@
 package com.marty.yummy.services.repository;
 
-import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
 import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
 
 import com.marty.yummy.dbutilities.AppDatabase;
 import com.marty.yummy.model.FoodDetails;
@@ -12,6 +13,7 @@ import com.marty.yummy.worker.SaveFoodMenu;
 import com.marty.yummy.worker.UpdateCart;
 
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,39 +21,43 @@ import retrofit2.Response;
 
 public class FoodRepository {
 
-    private static FoodRepository instance;
+    private static volatile FoodRepository instance;
     private static final String TAG = "FoodRepository";
 
-    private YummyAPIServices yummyAPIServices = APIClient.getClient().create(YummyAPIServices.class);
+    private final YummyAPIServices yummyAPIServices = APIClient.getClient().create(YummyAPIServices.class);
 
-    public MutableLiveData<Boolean> getFoodMenu(final Context context){
-
+    public MutableLiveData<Boolean> getFoodMenu(final Context context) {
         final MutableLiveData<Boolean> isFoodCallOngoing = new MutableLiveData<>();
         isFoodCallOngoing.setValue(true);
 
         yummyAPIServices.getFoodData().enqueue(new Callback<List<FoodDetails>>() {
             @Override
             public void onResponse(Call<List<FoodDetails>> call, Response<List<FoodDetails>> response) {
-                if(response.isSuccessful()) {
-                    new SaveFoodMenu(AppDatabase.getDatabase(context), response.body()).execute();
+                if (response.isSuccessful() && response.body() != null) {
+                    Executors.newSingleThreadExecutor().execute(() -> {
+                        AppDatabase db = AppDatabase.getDatabase(context);
+                        db.foodDetailsDao().insertAll(response.body());
+                        isFoodCallOngoing.postValue(false);
+                    });
+                } else {
+                    Log.e(TAG, "Response not successful: " + response.message());
                     isFoodCallOngoing.setValue(false);
-                }else{
-                    Log.e(TAG,"response not successful");
                 }
             }
 
             @Override
             public void onFailure(Call<List<FoodDetails>> call, Throwable t) {
-                Log.e(TAG,t.toString());
+                Log.e(TAG, "Failed to fetch food data: ", t);
+                isFoodCallOngoing.setValue(false);
             }
         });
         return isFoodCallOngoing;
     }
 
     public static FoodRepository getInstance() {
-        if(instance == null){
-            synchronized (FoodRepository.class){
-                if(instance == null){
+        if (instance == null) {
+            synchronized (FoodRepository.class) {
+                if (instance == null) {
                     instance = new FoodRepository();
                 }
             }
@@ -60,6 +66,8 @@ public class FoodRepository {
     }
 
     public void updateCart(final AppDatabase db, FoodDetails foodDetails) {
-        new UpdateCart(db).execute(foodDetails);
+        Executors.newSingleThreadExecutor().execute(() -> {
+            db.cartItemDao().updateCartWithFoodDetails(foodDetails);
+        });
     }
 }
